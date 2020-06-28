f0c40f95f94cc0742f26f548e52355bde55cf79d
ahlamalefishat96 <ahlam.alefishat@gmail.com>
2020-06-29 02:33:20 +0300

    lab-26

---
+++ resty/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/header/header.js';
+import Form from './components/form/form.js';
+import Footer from './components/footer/footer.js';
+
+function App() {
+  return (
+    <React.Fragment>
+    <Header/>
+    < Form/>
+    <Footer/>
+    </React.Fragment>
+  );
+}
+
+export default App;
