3a3a5da82ade0123128f339e69aafb8ebac2985e
ahlamalefishat96 <ahlam.alefishat@gmail.com>
2020-06-29 19:25:23 +0300

    Updates

---
+++ precache-manifest.846652a931b9d144964bf36e80b90824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047971c4cb1525258fb8cc6decfb53ff",
+    "url": "/AhlamAlefishat-401-advanced-javascript/resty/index.html"
+  },
+  {
+    "revision": "fcc45fa9dcff6f325039",
+    "url": "/AhlamAlefishat-401-advanced-javascript/resty/static/css/main.7602a872.chunk.css"
+  },
+  {
+    "revision": "3c3fe42eb5bed02bd8ed",
+    "url": "/AhlamAlefishat-401-advanced-javascript/resty/static/js/2.62d548b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AhlamAlefishat-401-advanced-javascript/resty/static/js/2.62d548b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc45fa9dcff6f325039",
+    "url": "/AhlamAlefishat-401-advanced-javascript/resty/static/js/main.78e13e09.chunk.js"
+  },
+  {
+    "revision": "fd2599676277d7f7a201",
+    "url": "/AhlamAlefishat-401-advanced-javascript/resty/static/js/runtime-main.e80ccefa.js"
+  }
+]);
